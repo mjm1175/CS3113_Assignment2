0fd021c787c910f5afdd1fdc8b2fc2c05c84b264
Jawn12345 <62352780+Jawn12345@users.noreply.github.com>
2021-11-05 12:24:38 -0400

    GameManager

--- Assets/Code/Game.cs
+++ Assets/Code/Game.cs
@@ -8,12 +8,18 @@ public class Game : MonoBehaviour
     private void Awake()
     {
         DontDestroyOnLoad(this);
-        Room start = new Room("SampleScene");
-        Room path0 = new Room("Path0");
-        Room path2 = new Room("Path2", prereqRooms: new Room[] { start });
+        //Room start = new Room("SampleScene");
+        //Room path0 = new Room("Path0");
+        //Room path2 = new Room("Path2", prereqRooms: new Room[] { start });
+
+        Room start = new Room("Corridor");
+        Room path0 = new Room("Room1");
+        Room path1 = new Room("Room2", prereqRooms: new Room[] { start });
+        Room path2 = new Room("Room3", prereqRooms: new Room[] { start });
+        
         Room boss = new Room("BossScene", prereqRooms: new Room[] { start, path0, path2 });
 
-        Rooms = new List<Room>() { start, path0, path2, boss };
+        Rooms = new List<Room>() { start, path0, path1, path2, boss };
 
         PublicVars.Game = this;
         start.Enter();
