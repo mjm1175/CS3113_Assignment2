85fc856c537ca1218b1f1bd2c8853aae3c226e0e
Maria <mjm1175@nyu.edu>
2021-11-07 14:31:50 -0500

    paper pickup in sample scene

---
+++ Assets/Code/PickupPaper.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickupPaper : MonoBehaviour
+{
+    public Text pickupText;
+    public Text storyText;
+
+    private void Start() {
+        pickupText.enabled = false;   
+        storyText.enabled = false; 
+    }
+
+    private void OnTriggerStay(Collider other) {
+        // TODO: add message for how to pickup
+        if (other.CompareTag("Player")){
+            pickupText.enabled = true;
+            if (Input.GetKeyDown("j")){
+                pickupText.enabled = false;
+                storyText.enabled = true; 
+                Destroy(gameObject);   
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")){
+            pickupText.enabled = false;
+        }
+    }
+}
