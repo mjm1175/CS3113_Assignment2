53169439712b8995644c22c2bb063359480e5ce6
Maria <mjm1175@nyu.edu>
2021-11-18 20:16:43 -0500

    removing quit button if webgl build

--- Assets/Code/UI/MenuScript.cs
+++ Assets/Code/UI/MenuScript.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject quitBtn;
+
+    private void Start() {
+        #if (UNITY_EDITOR)
+            quitBtn.SetActive(true);
+        #elif (UNITY_STANDALONE) 
+            quitBtn.SetActive(true);
+        #elif (UNITY_WEBGL)
+            quitBtn.SetActive(false);
+        #endif
+    }
+
     public void StartBtn()
     {
         // TODO: change if starting scene changes; make sure all public vars are reset
@@ -17,9 +30,6 @@ public class MenuScript : MonoBehaviour
             UnityEditor.EditorApplication.isPlaying = false;
         #elif (UNITY_STANDALONE) 
             Application.Quit();
-        #elif (UNITY_WEBGL)
-            // TODO: change to not show button at all
-            Application.OpenURL("about:blank");
         #endif
     }
 }
